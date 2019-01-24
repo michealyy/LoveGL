e96def5bee14b01c88cac385c66c87eaf415669c
陈勇星 <1037045972@qq.com>
2019-01-24 20:40:40 +0800

    +UI左边查看栏

--- src/ui/ui_root.cpp
+++ src/ui/ui_root.cpp
@@ -24,14 +24,8 @@ void UIRoot::Setup()
 {
     UIRect::Setup();
 
-    fps_label_ = new Label();
-    fps_label_->position = vec3(0, 40, 0);
-    AddChild(fps_label_);
-    draw_call_label_ = new Label();
-    draw_call_label_->position = vec3(0, 20, 0);
-    AddChild(draw_call_label_);
-    ui_draw_call_label_ = new Label();
-    AddChild(ui_draw_call_label_);
+    int width, height;
+    glfwGetWindowSize(Engine::GetInstance()->GetMainWindow(), &width, &height);
 
     //查看字体图集
     // auto font_texture = new UIRect();
@@ -41,17 +35,46 @@ void UIRoot::Setup()
     // font_texture->material = FontManager::GetInstance()->GetMaterial(0);
     // AddChild(font_texture);
 
-    auto ui1 = new Button();
-    ui1->name = "ui1";
-    ui1->SetImage("demo");
-    ui1->position = vec3(0, 200, 0);
-    AddChild(ui1);
+    //fps状态等
+    fps_label_ = new Label();
+    fps_label_->position = vec3(width - 100, height - 12, 0);
+    AddChild(fps_label_);
+    draw_call_label_ = new Label();
+    draw_call_label_->position = vec3(width - 100, height - 28, 0);
+    AddChild(draw_call_label_);
+    ui_draw_call_label_ = new Label();
+    ui_draw_call_label_->position = vec3(width - 100, height - 45, 0);
+    AddChild(ui_draw_call_label_);
 
-    // auto ui2 = new Button();
-    // ui2->name = "ui2";
-    // ui2->SetImage("demo");
-    // ui2->position = vec3(200, 0, 0);
-    // ui1->AddChild(ui2);
+    // auto ui1 = new Button();
+    // ui1->name = "ui1";
+    // ui1->SetImage("demo");
+    // ui1->position = vec3(0, 200, 0);
+    // AddChild(ui1);
+
+    //标题栏
+    auto ui_common_title_mat = new Material("ui_common_title");
+    ui_common_title_mat->SetShader("unlit_pos_tex");
+    ui_common_title_mat->SetTexture(0, "white");
+    auto title = new UIRect();
+    title->width = 100;
+    title->height = 30;
+    title->material = ui_common_title_mat;
+    title->color = vec3(1, 0, 0);
+    title->position = vec3(0, height - 30, 0);
+    AddChild(title);
+
+    //背景
+    auto ui_common_bg_mat = new Material("ui_common_bg");
+    ui_common_bg_mat->SetShader("unlit_pos_tex");
+    ui_common_bg_mat->SetTexture(0, "white");
+    auto bg = new UIRect();
+    bg->width = 100;
+    bg->height = (float)height - 30;
+    bg->material = ui_common_bg_mat;
+    bg->color = vec3(0.5, 0.5, 0.5);
+    bg->alpha = 0.8f;
+    AddChild(bg);
 }
 
 void UIRoot::Update(float deltaTime)
